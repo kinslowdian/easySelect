e82c9a05e92fb688f994551ac9a41407ac6564de
Simon Kinslow <simonkinslow@gmail.com>
2018-11-06 13:58:47 +0000

    Notes added

--- public/js/easySelect.js
+++ public/js/easySelect.js
@@ -10,6 +10,7 @@ function pageLoad_init()
 
 function test()
 {
+	// SAME AS JQUERY
 	e$(".box").addEventListener("click", test_event, false);
 }
 
@@ -17,6 +18,7 @@ function test_event(event)
 {
 	trace(event);
 
+	// RETURN SET CLASS / ID NAME RECORDED ON THE ELEMENT FROM THE FUNCTION
 	let targ = event.target.attributes["data-refselect"].value;
 
 	e$(targ).classList.add("new");
